2059b66c6ccdd6fe35f9d49bac99ca71cf59d242
csavelief <csavelief@gmail.com>
2013-05-18 10:59:57 +0200

    Refactoring.

--- test/fr/mncc/gwttoolbox/appengine/server/AppEngineVsPostgreSqlTest.java
+++ test/fr/mncc/gwttoolbox/appengine/server/AppEngineVsPostgreSqlTest.java
@@ -1,6 +1,38 @@
-package fr.mncc.postgres;
+/**
+ * Copyright (c) 2013 MNCC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * @author http://www.mncc.fr
+ */
+package fr.mncc.gwttoolbox.appengine.server;
 
-import static junit.framework.Assert.assertEquals;
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
+import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
+import fr.mncc.gwttoolbox.appengine.shared.SQuery2;
+import fr.mncc.gwttoolbox.appengine.shared.SQuery2.SClause2;
+import fr.mncc.gwttoolbox.appengine.shared.SQuery2.SFilter2;
+import fr.mncc.gwttoolbox.appengine.shared.SQuery2.SFilterOperator2;
+import fr.mncc.gwttoolbox.primitives.server.DateUtils;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
 import java.math.BigDecimal;
 import java.sql.Connection;
@@ -8,30 +40,9 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-
-import com.google.appengine.api.datastore.DatastoreService;
-import com.google.appengine.api.datastore.DatastoreServiceFactory;
-import com.google.appengine.api.datastore.Entity;
-import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
-import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
-
-import fr.mncc.gwttoolbox.appengine.server.DataStore2;
-import fr.mncc.gwttoolbox.appengine.server.JdbcPostgresConnection;
-import fr.mncc.gwttoolbox.appengine.server.PostgreSql2;
-import fr.mncc.gwttoolbox.appengine.shared.SQuery2;
-import fr.mncc.gwttoolbox.appengine.shared.SQuery2.SClause2;
-import fr.mncc.gwttoolbox.appengine.shared.SQuery2.SFilter2;
-import fr.mncc.gwttoolbox.appengine.shared.SQuery2.SFilterOperator2;
-import fr.mncc.gwttoolbox.primitives.server.DateUtils;
+import static junit.framework.Assert.assertEquals;
 
 public class AppEngineVsPostgreSqlTest {
 
