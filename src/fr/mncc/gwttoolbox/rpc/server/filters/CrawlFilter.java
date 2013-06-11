08f92a532acede482d437cafd03256512f55c5f8
csavelief <csavelief@gmail.com>
2013-06-11 17:37:25 +0200

    Migrate to HtmlUnit 2.12

--- src/fr/mncc/gwttoolbox/rpc/server/filters/CrawlFilter.java
+++ src/fr/mncc/gwttoolbox/rpc/server/filters/CrawlFilter.java
@@ -22,8 +22,6 @@ package fr.mncc.gwttoolbox.rpc.server.filters;
 
 import com.gargoylesoftware.htmlunit.*;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
-import fr.mncc.gwttoolbox.appengine.server.Memcache2;
-import fr.mncc.gwttoolbox.crypto.server.CryptoApi;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
@@ -38,12 +36,33 @@ import java.util.logging.Logger;
 /**
  * For AJAX crawlable websites
  */
-public final class CrawlFilter implements Filter {
+public class CrawlFilter implements Filter {
 
   public final static String SCHEME = "http";
-  public final static long PUMP_TIME = 5000;
-  public final static long WAIT_FOR = 2500;
+  public final static long PUMP_TIME = 500;
+  public final static long WAIT_FOR_JAVASCRIPT = 100;
   private final static Logger logger_ = Logger.getLogger(CrawlFilter.class.getCanonicalName());
+  private final static ThreadLocal<WebClient> webClient_ = new ThreadLocal<WebClient>() {
+
+    @Override
+    protected WebClient initialValue() {
+      WebClient webClient = new WebClient(BrowserVersion.FIREFOX_3_6);
+      webClient.setAjaxController(new AjaxController() {
+        @Override
+        public boolean processSynchron(HtmlPage page, WebRequest request, boolean async) {
+          return true;
+        }
+      });
+      webClient.setCssErrorHandler(new SilentCssErrorHandler());
+      webClient.getOptions().setThrowExceptionOnScriptError(false);
+      webClient.getOptions().setThrowExceptionOnFailingStatusCode(false);
+      webClient.getOptions().setCssEnabled(false);
+      webClient.getOptions().setPopupBlockerEnabled(false);
+      webClient.getOptions().setRedirectEnabled(false);
+      webClient.getOptions().setJavaScriptEnabled(true);
+      return webClient;
+    }
+  };
 
   @Override
   public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
@@ -51,69 +70,57 @@ public final class CrawlFilter implements Filter {
 
     HttpServletRequest req = (HttpServletRequest) request;
     String queryString = req.getQueryString();
+
     logger_.log(Level.INFO, "queryString=" + queryString);
     logger_.log(Level.INFO, "requestURL=" + req.getRequestURL());
 
     if (queryString != null && queryString.contains("_escaped_fragment_=")) {
 
-      String uri = req.getRequestURI();
-      int port = req.getServerPort();
-      String domain = req.getServerName();
+      final String uri = req.getRequestURI();
+      final int port = req.getServerPort();
+      final String domain = req.getServerName();
 
       // rewrite the URL back to the original #! version
       // remember to unescape any %XX characters
-      String url_with_hash_fragment = uri + rewriteQueryString(queryString);
+      final String url_with_hash_fragment = uri + rewriteQueryString(queryString);
 
       logger_.log(Level.INFO, "url_with_hash_fragment=" + url_with_hash_fragment);
 
       // and use the headless browser to obtain an HTML snapshot
-      URL url = new URL(SCHEME, domain, port, url_with_hash_fragment);
+      final URL url = new URL(SCHEME, domain, port, url_with_hash_fragment);
+      final WebRequest webRequest = new WebRequest(url);
 
       logger_.log(Level.INFO, "url=" + url.toString());
 
-      String staticSnapshotHtml =
-          (String) Memcache2.getSync("GOOGLEBOT", CryptoApi.hash(url.toString()));
+      String staticSnapshotHtml = getPageFromCache(url.toString());
       if (staticSnapshotHtml == null || staticSnapshotHtml.isEmpty()) {
 
         // Create webClient
-        WebClient webClient = new WebClient(BrowserVersion.FIREFOX_3_6);
-        webClient.setWebConnection(new UrlFetchWebConnection(webClient) {
+        webClient_.get().getCache().clear();
+        webClient_.get().setWebConnection(new UrlFetchWebConnection(webClient_.get()) {
           @Override
           public WebResponse getResponse(WebRequest request) throws IOException {
 
             final URL url = request.getUrl();
             final String file = url.getFile();
 
-            logger_.log(Level.INFO, url.toString());
-
-            if (!file.endsWith(".css") && !file.endsWith(".jpg") && !file.endsWith(".gif")
-                && !file.endsWith(".png"))
+            if (!ignoreFile(file)) {
+              logger_.log(Level.INFO, "*** Loaded : " + file);
               return super.getResponse(request);
+            }
+
+            logger_.log(Level.INFO, "*** Ignored : " + file);
             return new StringWebResponse("", request.getUrl());
           }
         });
-        webClient.setAjaxController(new AjaxController() {
-          @Override
-          public boolean processSynchron(HtmlPage page, WebRequest request, boolean async) {
-            return true;
-          }
-        });
-        webClient.getCache().clear();
-        webClient.setCssErrorHandler(new SilentCssErrorHandler());
-        webClient.setThrowExceptionOnScriptError(false);
-        webClient.setThrowExceptionOnFailingStatusCode(false);
-        webClient.setCssEnabled(false);
-        webClient.setPopupBlockerEnabled(false);
-        webClient.setRedirectEnabled(false);
-        webClient.setJavaScriptEnabled(true);
-
-        HtmlPage page = webClient.getPage(url);
+        final HtmlPage page = webClient_.get().getPage(webRequest);
 
         // Important! Give the headless browser enough time to execute JavaScript
         // The exact time to wait may depend on your application.
-        webClient.getJavaScriptEngine().pumpEventLoop(PUMP_TIME);
+        webClient_.get().getJavaScriptEngine().pumpEventLoop(PUMP_TIME);
 
-        int waitForBackgroundJavaScript = webClient.waitForBackgroundJavaScript(WAIT_FOR);
+        int waitForBackgroundJavaScript =
+            webClient_.get().waitForBackgroundJavaScript(WAIT_FOR_JAVASCRIPT);
         int counter = 0;
         while (waitForBackgroundJavaScript > 0 && counter++ < 5) {
           synchronized (page) {
@@ -123,15 +130,15 @@ public final class CrawlFilter implements Filter {
               logger_.log(Level.SEVERE, e.getMessage());
             }
           }
-          waitForBackgroundJavaScript = webClient.waitForBackgroundJavaScript(WAIT_FOR);
+          waitForBackgroundJavaScript =
+              webClient_.get().waitForBackgroundJavaScript(WAIT_FOR_JAVASCRIPT);
           logger_.log(Level.INFO, "Waiting...");
         }
 
         staticSnapshotHtml = page.asXml();
-        webClient.closeAllWindows();
+        webClient_.get().closeAllWindows();
 
-        Memcache2.put("GOOGLEBOT", CryptoApi.hash(url.toString()), staticSnapshotHtml,
-            7 * 24 * 60 * 60);
+        savePageToCache(url.toString(), staticSnapshotHtml);
       }
 
       logger_.log(Level.INFO, "page.asXml()=" + staticSnapshotHtml);
@@ -180,5 +187,23 @@ public final class CrawlFilter implements Filter {
       logger_.log(Level.SEVERE, e.toString());
       return "";
     }
+  }
+
+  /**
+   * Tell HtmlUnit whether to download or not a given file.
+   * 
+   * @param file
+   * @return true if HtmlUnit should download the file, false otherwise.
+   */
+  protected boolean ignoreFile(String file) {
+    return false;
+  }
+
+  protected void savePageToCache(String url, String page) {
+
+  }
+
+  protected String getPageFromCache(String url) {
+    return "";
   }
 }
